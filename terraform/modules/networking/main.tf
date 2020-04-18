493abd1c44c8d10d0ed26ffa89b257bf2bf7208c
Denis Silva <denisstortisilva@gmail.com>
2020-04-18 15:19:06 +1000

    Fix auto assign IP

--- terraform/modules/networking/main.tf
+++ terraform/modules/networking/main.tf
@@ -79,7 +79,7 @@ resource "aws_subnet" "public-subnet1" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "173.0.128.0/24"
   availability_zone = data.aws_availability_zones.available.names[0]
-
+  map_public_ip_on_launch = true
   tags = {
     Name = "${var.project_name}-public-sub1"
   }
@@ -89,7 +89,7 @@ resource "aws_subnet" "public-subnet2" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "173.0.129.0/24"
   availability_zone = data.aws_availability_zones.available.names[1]
-
+  map_public_ip_on_launch = true
   tags = {
     Name = "${var.project_name}-public-sub2"
   }
