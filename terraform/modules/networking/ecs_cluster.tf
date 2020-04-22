ee0b339ee4dfecb5d73af8b58f14d51106f958ce
Denis Silva <denisstortisilva@gmail.com>
2020-04-23 08:13:51 +1000

    Add ALB

--- terraform/modules/networking/ecs_cluster.tf
+++ terraform/modules/networking/ecs_cluster.tf
@@ -10,10 +10,68 @@ resource "aws_ecs_service" "wordpress" {
   desired_count   = 2
   launch_type     = "FARGATE"
   platform_version = "1.4.0"
+  
   network_configuration {
     subnets       = [aws_subnet.public-subnet1.id, aws_subnet.public-subnet2.id]
     security_groups  = [aws_default_security_group.allow_public_tcp80.id]
     assign_public_ip = true
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.wordpress.arn
+    container_name   = "wordpress"
+    container_port   = 80
+  }
+
+  depends_on = [
+    aws_lb_target_group.wordpress,
+  ]
+}
+
+resource "aws_lb_target_group" "wordpress" {
+  name     = "${var.project_name}-${var.environment}-wordpress-tg"
+  port     = 80
+  protocol = "HTTP"
+  target_type = "ip"
+  vpc_id   = aws_vpc.main.id
+
+  health_check {
+     enabled             = true
+     healthy_threshold   = 5
+     interval            = 30
+     matcher             = "200-399"
+     path                = "/"
+     port                = "traffic-port"
+     protocol            = "HTTP"
+     timeout             = 5
+     unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb" "wordpress" {
+  name               = "${var.project_name}-${var.environment}-wordpress-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = ["${aws_default_security_group.allow_public_tcp80.id}"]
+  subnets            = [aws_subnet.public-subnet1.id, aws_subnet.public-subnet2.id]
+
+  enable_deletion_protection = true
+
+  tags = {
+    Environment = var.environment
+    Project = var.project_name
+  }
+}
+
+resource "aws_lb_listener" "wordpress" {
+  load_balancer_arn = aws_lb.wordpress.arn
+  port              = "80"
+  protocol          = "HTTP"
+  # ssl_policy        = "ELBSecurityPolicy-2016-08"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.wordpress.arn
   }
 }
 
